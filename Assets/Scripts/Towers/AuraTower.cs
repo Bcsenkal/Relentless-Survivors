c1719c0f046ab74ef89e145442d417d89dab16be
Bcsenkal <98615099+Bcsenkal@users.noreply.github.com>
2022-05-06 12:19:46 +0300

    refactor auratower
    moved the code(which informs other towers to get new aura) to base class.
    we don't need to repeat same code for every subclass of AuraTower

--- Assets/Scripts/Towers/AuraTower.cs
+++ Assets/Scripts/Towers/AuraTower.cs
@@ -6,8 +6,10 @@ public class AuraTower : Tower, IAuraProvider
 {
     public virtual void ApplyAura(GameObject tower)
     {
+        tower.GetComponent<Tower>().currentAuras.Add(gameObject.name);
     }
     public virtual void RemoveAura(GameObject tower)
     {
+        tower.GetComponent<Tower>().currentAuras.Remove(gameObject.name);
     }
 }
